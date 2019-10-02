7854b3dbe4b494a26354d9692982f4f47e5a3654
toto <message@example.com>
2019-10-02 17:15:40 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { PortfolioOneComponent } from './portfolio-one/portfolio-one.component';
+import { PortfolioUpdateComponent } from './portfolio-update/portfolio-update.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    PortfolioComponent,
+    PortfolioOneComponent,
+    PortfolioUpdateComponent,
+    NotfoundComponent,
+    ContactComponent,
+    HomepageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
